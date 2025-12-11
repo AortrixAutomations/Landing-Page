43fcb431c1ae54b71044e9b626a93170b99ac7e2
AortrixAutomations <support@aortrix.com>
2025-12-11 19:01:37 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/scripts/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: { aortrix: "#2ECC71" }
+    }
+  },
+  plugins: []
+}
